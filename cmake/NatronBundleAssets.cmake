eeeb9370da139e94f4d26f241f0d5042affaa30f
t <t@t.t>
2026-09-04 04:49:45 -0400

    build: install fontconfig tree and PyPlugs into the bundle layout
    Process fonts.conf.in into fonts.conf (replacing $$FC_DEFAULT_FONTS and
    $$FC_CACHEDIR with system defaults) and install it with conf.d/ to
    Resources/etc/fonts/ where AppManager.cpp expects it. Install PyPlugs
    to Plugins/ for PythonUtils.cpp's PYTHONPATH setup.

--- cmake/NatronBundleAssets.cmake
+++ cmake/NatronBundleAssets.cmake
@@ -22,4 +22,21 @@ if(NATRON_BUNDLE_ASSETS)
     install(DIRECTORY "${NATRON_ASSETS_DIR}/OpenColorIO-Configs"
         DESTINATION "Resources"
     )
+
+    file(READ "${CMAKE_CURRENT_SOURCE_DIR}/Gui/Resources/etc/fonts/fonts.conf.in" fonts_conf_content)
+    string(REPLACE "$$FC_DEFAULT_FONTS" "<dir>/usr/share/fonts</dir>" fonts_conf_content "${fonts_conf_content}")
+    string(REPLACE "$$FC_CACHEDIR" "<cachedir>/var/cache/fontconfig</cachedir>" fonts_conf_content "${fonts_conf_content}")
+    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/fonts.conf" "${fonts_conf_content}")
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/fonts.conf"
+        DESTINATION "Resources/etc/fonts"
+    )
+    install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/Gui/Resources/etc/fonts/conf.d"
+        DESTINATION "Resources/etc/fonts"
+    )
+
+    # AppManager.cpp:1708's absolute /usr/share/Natron/Plugins is deliberately unsatisfied by a relocatable prefix
+    install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/Gui/Resources/PyPlugs/"
+        DESTINATION "Plugins"
+    )
+
 endif()

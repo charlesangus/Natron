2f9fdc7c52f997837824aa47676dcaed77152cd0
t <t@t.t>
2026-09-18 08:44:39 -0400

    Rename ensureUnknownChocieIsNotInternalPlaneID to ensureUnknownChoiceIsNotInternalLayerID

--- Gui/KnobGuiChoice.cpp
+++ Gui/KnobGuiChoice.cpp
@@ -362,20 +362,21 @@ KnobGuiChoice::updateToolTip()
 // The project only saves the choice ID that was selected and not the associated label.
 // If the ID cannot be found in the menu upon loading the project, then the ID of the choice
 // will be displayed in the menu.
-// For a plane selector, if the plane is not present in the menu when loading the project it will display
-// the raw ID of the plane which contains stuff that should not be displayed to the user.
-static void ensureUnknownChocieIsNotInternalPlaneID(QString& label)
+// For a layer selector, if the layer is not present in the menu when loading the project it will display
+// the raw ID of the layer which contains stuff that should not be displayed to the user.
+static void
+ensureUnknownChoiceIsNotInternalLayerID(QString& label)
 {
     if (label.contains(QLatin1String(kNatronColorPlaneID))) {
-        label.replace(QLatin1String(kNatronColorPlaneID), QLatin1String(kNatronColorPlaneLabel)); ;
+        label.replace(QLatin1String(kNatronColorPlaneID), QLatin1String(kNatronColorPlaneLabel));
     } else if (label.contains(QLatin1String(kNatronBackwardMotionVectorsPlaneID))) {
-        label.replace(QLatin1String(kNatronBackwardMotionVectorsPlaneID), QLatin1String(kNatronBackwardMotionVectorsPlaneLabel)); ;
+        label.replace(QLatin1String(kNatronBackwardMotionVectorsPlaneID), QLatin1String(kNatronBackwardMotionVectorsPlaneLabel));
     } else if (label.contains(QLatin1String(kNatronForwardMotionVectorsPlaneID))) {
-        label.replace(QLatin1String(kNatronForwardMotionVectorsPlaneID), QLatin1String(kNatronForwardMotionVectorsPlaneLabel)); ;
+        label.replace(QLatin1String(kNatronForwardMotionVectorsPlaneID), QLatin1String(kNatronForwardMotionVectorsPlaneLabel));
     } else if (label.contains(QLatin1String(kNatronDisparityLeftPlaneID))) {
-        label.replace(QLatin1String(kNatronDisparityLeftPlaneID), QLatin1String(kNatronDisparityLeftPlaneLabel)); ;
+        label.replace(QLatin1String(kNatronDisparityLeftPlaneID), QLatin1String(kNatronDisparityLeftPlaneLabel));
     } else if (label.contains(QLatin1String(kNatronDisparityRightPlaneID))) {
-        label.replace(QLatin1String(kNatronDisparityRightPlaneID), QLatin1String(kNatronDisparityRightPlaneLabel)); ;
+        label.replace(QLatin1String(kNatronDisparityRightPlaneID), QLatin1String(kNatronDisparityRightPlaneLabel));
     }
 
 }
@@ -408,7 +409,7 @@ KnobGuiChoice::updateGUI(int /*dimension*/)
     if ( _comboBox->isCascading() || activeEntry.id.empty() ) {
         _comboBox->setCurrentIndex_no_emit( knob->getValue() );
     } else {
-        ensureUnknownChocieIsNotInternalPlaneID(activeEntryLabel);
+        ensureUnknownChoiceIsNotInternalLayerID(activeEntryLabel);
         _comboBox->setCurrentText_no_emit( activeEntryLabel );
     }
 }
